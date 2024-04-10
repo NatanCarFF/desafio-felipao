3da6b46c53b5bfd2ee26e1912900cb6bf79182cb
NatanCarFF <natanael.wlan@gmail.com>
2024-04-10 16:18:13 -0300

    atualização 100420241618

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
-let nome = "felipe";
-console.log(nome);
+# 1️⃣ Desafio Classificador de nível de Herói
+
+var nomeDoHeroi = "NatanCarFF"
+var xpDoHeroi = 0
+
+switch (xpDoHeroi) {
+    case (xpDoHeroi <= 1000)
+    console.log("O Herói de nome "nomeDoHeroi"está no nível de Ferro")
+        break
+
+    case (xpDoHeroi <= 2000)
+    console.log("Bronze")
+        break
+
+    case (xpDoHeroi <= 5000)
+    console.log("Prata")
+        break
+
+    case (xpDoHeroi <= 7000)
+    console.log("Ouro")
+        break
+
+    case (xpDoHeroi <= 8000)
+    console.log("Platina")
+        break
+
+    case (xpDoHeroi <= 9000)
+    console.log("Ascendente")
+        break
+
+    case (xpDoHeroi <= 10000)
+    console.log("Imortal")
+        break
+
+    case (xpDoHeroi > 10000)
+    console.log("Radiante")
+        break
+
+}
